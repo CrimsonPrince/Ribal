2f4ad83dcdcceed88433a2c83fc9ecb2ffec9120
Arthur Coll <arthur.coll@oracle.com>
2024-01-04 19:53:16 +0000

    Rebrand to yellowbucket.ie

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 module.exports = {
-  siteUrl: 'reactresume.com',
+  siteUrl: 'yellowbucket.ie',
   exclude: ['/404*', '/500*'],
   transform: async (config, path) => {
     return {
